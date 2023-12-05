c2ce589f12379e26756791fa972ad00a09a81d90
killshotroxs <krishnal@hotmail.com>
2023-12-05 16:41:42 +0530

    Deploying Project

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://cwy:test12345@cluster0.g6cbab9.mongodb.net/mern?retryWrites=true&w=majority"
+);
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}).then(function(users){
+        res.json(users)
+    }).catch(function(err){
+        res.json(err)
+    })
+})
+
+app.post("/createUser", async (req,res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+    res.json(user);
+})
+
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
